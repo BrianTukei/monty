773af93874bf970b99851f9440183723f9e8beb3
BrianTukei <briantukei1000@gmail.com>
2024-01-19 03:38:28 -0800

    Monty

--- add.c
+++ add.c
@@ -4,7 +4,7 @@
  * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
@@ -30,3 +30,4 @@ void f_add(stack_t **head, unsigned int counter)
 	*head = h->next;
 	free(h);
 }
+
